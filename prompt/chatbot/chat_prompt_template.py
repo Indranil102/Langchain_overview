f3630882941feffa8196b7db52cb4e1313148f2c
Indranil102e <indranilsamanta2003@gmail.com>
2026-03-09 18:18:02 +0530

    structured output file

--- prompt/chatbot/chat_prompt_template.py
+++ prompt/chatbot/chat_prompt_template.py
@@ -1,7 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-
-
 chatTemplate= ChatPromptTemplate([
     ("system", "You are a helpful {domain} assistant "),
     ("human", "Explain the topic {question}"),
