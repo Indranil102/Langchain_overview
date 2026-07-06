dbc2059e311f94c4816f3b72fd136c5f04f0122c
Indranil102e <indranilsamanta2003@gmail.com>
2026-07-06 17:32:54 +0530

    Update page.py to print only the content of the first document loaded from the web

--- course_mate_Ai/document loaders/page.py
+++ course_mate_Ai/document loaders/page.py
@@ -4,4 +4,4 @@ from langchain_community.document_loaders import WebBaseLoader
 data=WebBaseLoader("https://www.apple.com/in/macbook-air/")
 docs=data.load()
 
-print(docs) 
+print(docs[0].page_content) 
